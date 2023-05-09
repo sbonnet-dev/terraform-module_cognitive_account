172b6416299aba80955ec49fad117854c5cd8615
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-09 15:12:13 +0200

    new v

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_cognitive_account" "cognitive_account" {
+  name = var.name
+
+  location            = var.location
+  resource_group_name = var.rg_name
+
+
+  custom_subdomain_name = var.custom_subdomain_name//"cog-fr-oai"
+  kind                  = var.kind//"FormRecognizer"
+  sku_name              = var.sku_name //"S0"
+
+  tags = {
+    project     = var.project
+    environment = var.environment
+    owner       = var.owner
+  }
+
+  depends_on = [
+    module.resource_group
+  ]
+}
