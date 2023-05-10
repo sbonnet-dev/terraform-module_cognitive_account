936c35a3a4852020292c02b16c564307534536c6
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-10 13:28:05 +0200

    added var + output

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_cognitive_account" "cognitive_account" {
   name     = lower(format("ca-%s-%s-%s", var.project, var.environment, var.name))
+  custom_subdomain_name = lower(format("ca-%s-%s-%s", var.project, var.environment, var.name))//var.custom_subdomain_name//"cog-fr-oai"
 
   location            = var.location
   resource_group_name = var.rg_name
 
 
-  custom_subdomain_name = var.custom_subdomain_name//"cog-fr-oai"
   kind                  = var.kind//"FormRecognizer"
   sku_name              = var.sku_name //"S0"
 
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "id" {
   value = azurerm_cognitive_account.cognitive_account.id
 }
+
+output "id" {
+  name = azurerm_cognitive_account.cognitive_account.name
+}
